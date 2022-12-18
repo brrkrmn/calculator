d54c61d1c232862d3282ec91bffbdbcd93744bcf
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-18 17:03:18 +0300

    add showDisplay

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-const add = (x, y) => x + y;
-const subtract = (x, y) => x - y;
-const multiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
+function addEventToDigits(screenText) {
+    const buttons = document.querySelectorAll(".digit");
+    for (const button of buttons) {
+        button.addEventListener("click", () => {
+            displayValue += button.value;
+            screenText.textContent = displayValue;
+        });
+    };
+}
 
-function operate() {
-    
+function addEventToClearButton(screenText) {
+    const clearButton = document.querySelector(".clear-button");
+    clearButton.addEventListener("click", () => {
+        displayValue = "";
+        screenText.textContent = displayValue;
+    });
+}
+
+function showDisplay() {
+    let screenText = document.querySelector(".screen");
+    addEventToClearButton(screenText);
+    addEventToDigits(screenText);
+}
+
+let displayValue = "";   
+showDisplay();
+
+
+const add = (number1, number2) => number1 + number2;
+const subtract = (number1, number2) => number1 - number2;
+const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number1 / number2;
+
+function operate(number1, operator, number2) {
+    if (operator === "+") {
+        add(number1, number2);
+    } else if (operator === "-") {
+        subtract(number1 - number2);
+    } else if (operator === "x") {
+        multiply(number1, number2);
+    } else if (operator === "÷") {
+        divide(number1, number2);
+    }
 }
