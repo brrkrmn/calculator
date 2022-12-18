28f4506c15de6d7b6bce1e7c3935c98c343adcfb
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-19 00:15:14 +0300

    add js

--- index.js
+++ index.js
@@ -1,3 +1,68 @@
+function getResult(operation) {
+    while (findFirstOperatorIndex(operation) !== -1) {
+        let firstOperatorIndex = findFirstOperatorIndex(operation);
+        operation = calculate(operation, firstOperatorIndex)
+    }
+    return operation;
+}
+  
+function findFirstOperatorIndex(operation) {
+    const operators = ["x", "÷", "+", "-"];
+    let firstOperatorIndex = -1;
+    for (let operator of operators) {
+      if (operation.includes(operator)) {
+        firstOperatorIndex = operation.indexOf(operator);
+        break;
+      }
+    }
+    return firstOperatorIndex;
+}
+  
+function findLeftNumber(operation, firstOperatorIndex) {
+    let leftNumber = "";
+    let currentIndex = firstOperatorIndex -1;
+    const operators = ["+", "-", "x", "÷", undefined];
+    while(!operators.includes(operation[currentIndex])) {
+      leftNumber += operation[currentIndex];
+      currentIndex--;
+    }
+    return parseInt(leftNumber.split("").reverse().join(''));
+}
+  
+function findRightNumber(operation, firstOperatorIndex) {
+    let rightNumber = "";
+    let currentIndex = firstOperatorIndex + 1;
+    const operators = ["+", "-", "x", "÷", undefined];
+    while (!operators.includes(operation[currentIndex])) {
+      rightNumber += operation[currentIndex];
+      currentIndex++;
+    }
+    return parseInt(rightNumber);
+}
+  
+function operate(leftNumber, rightNumber, operator) {
+    let result = "";
+    if (operator === "+") {
+      result = leftNumber + rightNumber;
+    } else if (operator === "-") {
+      result = leftNumber - rightNumber;
+    } else if (operator === "x") {
+      result = leftNumber * rightNumber;
+    } else {
+      result = leftNumber / rightNumber;
+    }
+    return result;
+}
+  
+function calculate(operation, firstOperatorIndex){
+    let leftNumber = findLeftNumber(operation, firstOperatorIndex);
+    let rightNumber = findRightNumber(operation, firstOperatorIndex);
+    let operator = operation[firstOperatorIndex];
+    let result = operate(leftNumber, rightNumber, operator);
+    let currentOperation = leftNumber + operator + rightNumber;
+    return operation.replace(currentOperation, result);
+}
+
 function addEventToDigits(screenText) {
     const buttons = document.querySelectorAll(".digit");
     for (const button of buttons) {
@@ -16,29 +81,19 @@ function addEventToClearButton(screenText) {
     });
 }
 
+function addEventToResultButton(screenText) {
+    let resultButton = document.querySelector(".result-button");
+    resultButton.addEventListener("click", () => {
+        screenText.textContent += "=" + (getResult(screenText.textContent));
+    })
+}
+
 function showDisplay() {
     let screenText = document.querySelector(".screen");
     addEventToClearButton(screenText);
     addEventToDigits(screenText);
+    addEventToResultButton(screenText);
 }
 
 let displayValue = "";   
 showDisplay();
-
-
-const add = (number1, number2) => number1 + number2;
-const subtract = (number1, number2) => number1 - number2;
-const multiply = (number1, number2) => number1 * number2;
-const divide = (number1, number2) => number1 / number2;
-
-function operate(number1, operator, number2) {
-    if (operator === "+") {
-        add(number1, number2);
-    } else if (operator === "-") {
-        subtract(number1 - number2);
-    } else if (operator === "x") {
-        multiply(number1, number2);
-    } else if (operator === "÷") {
-        divide(number1, number2);
-    }
-}
